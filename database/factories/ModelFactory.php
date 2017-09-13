b17679b8d66e20fc6d3e86c6325ecfed5d4ebe16
at-tinhnguyen <tinh.nguyen@asiantech.vn>
2017-09-14 06:34:09 +0700

    046. Creating The Factory for Product

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -2,6 +2,7 @@
 
 use App\User;
 use App\Category;
+use App\Product;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,5 +34,16 @@ $factory->define(Category::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->word,
         'description' => $faker->paragraph(1),
+    ];
+});
+
+$factory->define(Product::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity' => $faker->randomElement([Product::AVAILABLE_PRODUCT, Product::UNAVAILABLE_PRODUCT]),
+        'image' => $faker->randomElement(['1.jpg', '2.jpg', '3.jpg']),
+        'seller_id' => User::all()->random()->id,
+        // User::inRandomOrder()->first()->id
     ];
 });
