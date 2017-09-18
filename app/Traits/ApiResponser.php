76f409ef7f6b3ecb238f30238223ce0578c8cd31
at-tinhnguyen <tinh.nguyen@asiantech.vn>
2017-09-18 11:18:52 +0700

    062. Using The Generalized Methods for Error Responses

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -14,7 +14,7 @@ trait ApiResponser
 
 	protected function errorResponse($message, $code)
 	{
-		return response()->json(['error' => $message, $code => $code], $code);
+		return response()->json(['error' => $message, 'code' => $code], $code);
 	}
 
 	protected function showAll(Collection $collection, $code = 200)
