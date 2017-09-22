462ee5963b2a2424960ff6f4ae58971ccc596913
at-tinhnguyen <tinh.nguyen@asiantech.vn>
2017-09-22 16:02:37 +0700

    076. Using soft Deleting for All the Models

--- app/Product.php
+++ app/Product.php
@@ -6,12 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 use App\Seller;
 use App\Category;
 use App\Transaction;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
+	use SoftDeletes;
+
     const UNAVAILABLE_PRODUCT = 'unavailable';
     const AVAILABLE_PRODUCT = 'available';
 
+    protected $dates = ['deleted_at'];
     protected $fillable = [
       'name',
       'description',
