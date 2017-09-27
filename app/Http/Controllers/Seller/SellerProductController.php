0a8eaddcb21bb65388820fd59079deb9abdf7c65
at-tinhnguyen <tinh.nguyen@asiantech.vn>
2017-09-27 14:25:51 +0700

    101. Implementing the Store Method for SellerProductController

--- app/Http/Controllers/Seller/SellerProductController.php
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Seller;
 use App\Seller;
 use Illuminate\Http\Request;
 use App\Http\Controllers\ApiController;
+use App\Product;
+use App\User;
 
 class SellerProductController extends ApiController
 {
@@ -18,5 +20,56 @@ class SellerProductController extends ApiController
         $products = $seller->products;
 
         return $this->showAll($products);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, User $seller)
+    {
+        $rules = [
+            'name' => 'required',
+            'description' => 'required',
+            'quantity' => 'required|integer|min:1',
+            'image' => 'required|image'
+        ];
+        
+        $this->validate($request, $rules);
+        
+        $data = $request->all();
+
+        $data['status'] = Product::UNAVAILABLE_PRODUCT;
+        $data['image'] = '1.jpg';
+        $data['seller_id'] = $seller->id;
+        
+        $product = Product::create($data);
+        
+        return $this->showOne($product);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Seller  $seller
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Seller $seller)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Seller  $seller
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Seller $seller)
+    {
+        //
     }
 }
