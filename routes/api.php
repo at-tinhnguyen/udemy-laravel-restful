7fe3ed21dd256602904b904893b335d873e12e89
at-tinhnguyen <tinh.nguyen@asiantech.vn>
2017-09-27 16:12:28 +0700

    104. [Challenge] Implementing the Index Operation of ProductTransaction

--- routes/api.php
+++ routes/api.php
@@ -35,6 +35,7 @@ Route::resource('categories.buyers', 'Category\CategoryBuyerController', ['only'
 * Products
 */
 Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
+Route::resource('products.transactions', 'Product\ProductTransactionController', ['only' => ['index']]);
 
 /**
 * Sellers
