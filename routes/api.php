ff3dc046357c6376f9a02d1eb86135effeea0dd7
at-tinhnguyen <tinh.nguyen@asiantech.vn>
2017-09-27 11:30:42 +0700

    099. Implementing the Index Method for SellerBuyerController

--- routes/api.php
+++ routes/api.php
@@ -42,6 +42,7 @@ Route::resource('products', 'Product\ProductController', ['only' => ['index', 's
 Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
 Route::resource('sellers.transactions', 'Seller\SellerTransactionController', ['only' => ['index']]);
 Route::resource('sellers.categories', 'Seller\SellerCategoryController', ['only' => ['index']]);
+Route::resource('sellers.buyers', 'Seller\SellerBuyerController', ['only' => ['index']]);
 
 /**
 * Transactions
