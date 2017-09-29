c365c0c1714857848b26dae26bee8173c8b33d85
at-tinhnguyen <tinh.nguyen@asiantech.vn>
2017-09-29 11:08:59 +0700

    116. Implementing the Verification System

--- app/User.php
+++ app/User.php
@@ -41,7 +41,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
-        'verification_token',
+        // 'verification_token',
 
     ];
 
