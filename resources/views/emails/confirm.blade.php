16bc8cbfc31eb3d20ddadc24a27d236b9e26fc86
at-tinhnguyen <tinh.nguyen@asiantech.vn>
2017-10-02 09:17:13 +0700

    126. Using Markdown Mailables

--- resources/views/emails/confirm.blade.php
+++ resources/views/emails/confirm.blade.php
@@ -1,3 +1,11 @@
-Hello {{ $user->name }}
-You has change email. Please verify your new email using this link:
-{{ route('verify', $user->verification_token) }} 
+@component('mail::message')
+# Hello {{$user->name}}
+You has change email. Please verify your new email using this button:
+
+@component('mail::button', ['url' => route('verify', $user->verification_token)])
+Verify Account
+@endcomponent
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent 
