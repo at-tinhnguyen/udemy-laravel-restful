1d945a8d624834619a6278ba1903894f5ab1444e
at-tinhnguyen <tinh.nguyen@asiantech.vn>
2017-10-04 16:56:34 +0700

    138. Linking the Models with its Respective Transformer

--- app/Category.php
+++ app/Category.php
@@ -5,11 +5,13 @@ namespace App;
 use App\Product;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Transformers\CategoryTransformer;
 
 class Category extends Model
 {
 	use SoftDeletes;
 
+    public $transformer = CategoryTransformer::class;
     protected $dates = ['deleted_at'];
     protected $fillable = [
       'name',
--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Transformers\UserTransformer;
 
 class User extends Authenticatable
 {
@@ -15,6 +16,8 @@ class User extends Authenticatable
 
     const ADMIN_USER = 'true';
     const REGULAR_USER = 'false';
+
+    public $transformer = UserTransformer::class;
 
     protected $table = 'users';
     protected $dates = ['deleted_at'];
